6a14fb7735507431b8f4564daf3da6fd03df5990
Stephen L <lrq3000@gmail.com>
2016-04-15 01:08:34 +0200

    Add Ascii Path Renamer (asciirename.py) script
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

---
+++ asciirename/asciirename.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python
+#
+# asciirename.py
+# Copyright (C) 2016 Larroque Stephen
+#
+# Licensed under the MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#=================================
+#              Ascii Path Renamer
+#                    Python 2.7.11
+#                by Stephen Larroque
+#                     License: MIT
+#            Creation date: 2016-04-15
+#=================================
+#
+#
+# TODO:
+# * Nothing here.
+#
+
+from __future__ import print_function
+
+__version__ = '0.2'
+
+import argparse
+import os
+import unidecode  # to convert unicode accentuated strings to ascii
+import shlex
+import shutil
+import sys
+
+try:
+    from scandir import walk # use the faster scandir module if available (Python >= 3.5), see https://github.com/benhoyt/scandir
+except ImportError:
+    from os import walk # else, default to os.walk()
+
+try:
+    _str = basestring
+except NameError:
+    _str = str
+
+
+
+#***********************************
+#                       AUX
+#***********************************
+
+def is_file(dirname):
+    '''Checks if a path is an actual file that exists'''
+    if not os.path.isfile(dirname):
+        msg = "{0} is not an existing file".format(dirname)
+        raise ArgumentTypeError(msg)
+    else:
+        return dirname
+
+def is_dir(dirname):
+    '''Checks if a path is an actual directory that exists'''
+    if not os.path.isdir(dirname):
+        msg = "{0} is not a directory".format(dirname)
+        raise ArgumentTypeError(msg)
+    else:
+        return dirname
+
+def is_dir_or_file(dirname):
+    '''Checks if a path is an actual directory that exists or a file'''
+    if not os.path.isdir(dirname) and not os.path.isfile(dirname):
+        msg = "{0} is not a directory nor a file".format(dirname)
+        raise ArgumentTypeError(msg)
+    else:
+        return dirname
+
+def fullpath(relpath):
+    '''Relative path to absolute'''
+    if (type(relpath) is object or hasattr(relpath, 'read')): # relpath is either an object or file-like, try to get its name
+        relpath = relpath.name
+    return os.path.abspath(os.path.expanduser(relpath))
+
+def recwalk(inputpath, sorting=True, topdown=True):
+    '''Recursively walk through a folder. This provides a mean to flatten out the files restitution (necessary to show a progress bar). This is a generator.'''
+    # If it's only a single file, return this single file
+    if os.path.isfile(inputpath):
+        abs_path = fullpath(inputpath)
+        yield os.path.dirname(abs_path), os.path.basename(abs_path)
+    # Else if it's a folder, walk recursively and return every files
+    else:
+        for dirpath, dirs, files in walk(inputpath, topdown=topdown):	
+            if sorting:
+                files.sort()
+                dirs.sort() # sort directories in-place for ordered recursive walking
+            for filename in files:
+                yield (dirpath, filename) # return directory (full path) and filename
+            for folder in dirs:
+                yield (dirpath, folder)
+
+def remove_if_exist(path):  # pragma: no cover
+    """Delete a file or a directory recursively if it exists, else no exception is raised"""
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+            return True
+        elif os.path.isfile(path):
+            os.remove(path)
+            return True
+    return False
+
+def create_dir_if_not_exist(path):  # pragma: no cover
+    """Create a directory if it does not already exist, else nothing is done and no error is return"""
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def copy_any(src, dst, only_missing=False):  # pragma: no cover
+    """Copy a file or a directory tree, deleting the destination before processing"""
+    if not only_missing:
+        remove_if_exist(dst)
+    if os.path.exists(src):
+        if os.path.isdir(src):
+            if not only_missing:
+                shutil.copytree(src, dst, symlinks=False, ignore=None)
+            else:
+                for dirpath, filepath in recwalk(src):
+                    srcfile = os.path.join(dirpath, filepath)
+                    relpath = os.path.relpath(srcfile, src)
+                    dstfile = os.path.join(dst, relpath)
+                    if not os.path.exists(dstfile):
+                        create_dir_if_not_exist(os.path.dirname(dstfile))
+                        shutil.copyfile(srcfile, dstfile)
+                        shutil.copystat(srcfile, dstfile)
+            return True
+        elif os.path.isfile(src) and (not only_missing or not os.path.exists(dst)):
+            create_dir_if_not_exist(os.path.dirname(dst))
+            shutil.copyfile(src, dst)
+            shutil.copystat(src, dst)
+            return True
+    return False
+
+
+
+#***********************************
+#        GUI AUX FUNCTIONS
+#***********************************
+
+# Try to import Gooey for GUI display, but manage exception so that we replace the Gooey decorator by a dummy function that will just return the main function as-is, thus keeping the compatibility with command-line usage
+try:  # pragma: no cover
+    import gooey
+except ImportError as exc:
+    # Define a dummy replacement function for Gooey to stay compatible with command-line usage
+    class gooey(object):  # pragma: no cover
+        def Gooey(func):
+            return func
+    # If --gui was specified, then there's a problem
+    if len(sys.argv) > 1 and sys.argv[1] == '--gui':  # pragma: no cover
+        print('ERROR: --gui specified but an error happened with lib/gooey, cannot load the GUI (however you can still use this script in commandline). Check that lib/gooey exists and that you have wxpython installed. Here is the error: ')
+        raise(exc)
+
+def conditional_decorator(flag, dec):  # pragma: no cover
+    def decorate(fn):
+        if flag:
+            return dec(fn)
+        else:
+            return fn
+    return decorate
+
+def check_gui_arg():  # pragma: no cover
+    '''Check that the --gui argument was passed, and if true, we remove the --gui option and replace by --gui_launched so that Gooey does not loop infinitely'''
+    if len(sys.argv) > 1 and sys.argv[1] == '--gui':
+        # DEPRECATED since Gooey automatically supply a --ignore-gooey argument when calling back the script for processing
+        #sys.argv[1] = '--gui_launched' # CRITICAL: need to remove/replace the --gui argument, else it will stay in memory and when Gooey will call the script again, it will be stuck in an infinite loop calling back and forth between this script and Gooey. Thus, we need to remove this argument, but we also need to be aware that Gooey was called so that we can call gooey.GooeyParser() instead of argparse.ArgumentParser() (for better fields management like checkboxes for boolean arguments). To solve both issues, we replace the argument --gui by another internal argument --gui_launched.
+        return True
+    else:
+        return False
+
+def AutoGooey(fn):  # pragma: no cover
+    '''Automatically show a Gooey GUI if --gui is passed as the first argument, else it will just run the function as normal'''
+    if check_gui_arg():
+        return gooey.Gooey(fn)
+    else:
+        return fn
+
+
+
+#***********************************
+#                       MAIN
+#***********************************
+
+@AutoGooey
+def main(argv=None):
+    if argv is None: # if argv is empty, fetch from the commandline
+        argv = sys.argv[1:]
+    elif isinstance(argv, _str): # else if argv is supplied but it's a simple string, we need to parse it to a list of arguments before handing to argparse or any other argument parser
+        argv = shlex.split(argv) # Parse string just like argv using shlex
+
+    #==== COMMANDLINE PARSER ====
+
+    #== Commandline description
+    desc = '''Ascii Path Renamer v%s
+Description: Rename all directories/files names from unicode (ie, accentuated characters) to ascii.
+
+Note: use --gui (without any other argument) to launch the experimental gui (needs Gooey library).
+    ''' % __version__
+    ep = ''' '''
+
+    #== Commandline arguments
+    #-- Constructing the parser
+    # Use GooeyParser if we want the GUI because it will provide better widgets
+    if len(argv) > 0 and (argv[0] == '--gui' and not '--ignore-gooey' in argv):  # pragma: no cover
+        # Initialize the Gooey parser
+        main_parser = gooey.GooeyParser(add_help=True, description=desc, epilog=ep, formatter_class=argparse.RawTextHelpFormatter)
+        # Define Gooey widget types explicitly (because type auto-detection doesn't work quite well)
+        widget_dir = {"widget": "DirChooser"}
+        widget_filesave = {"widget": "FileSaver"}
+        widget_file = {"widget": "FileChooser"}
+        widget_text = {"widget": "TextField"}
+    else: # Else in command-line usage, use the standard argparse
+        # Delete the special argument to avoid unrecognized argument error in argparse
+        if len(argv) > 0 and '--ignore-gooey' in argv[0]: argv.remove('--ignore-gooey') # this argument is automatically fed by Gooey when the user clicks on Start
+        # Initialize the normal argparse parser
+        main_parser = argparse.ArgumentParser(add_help=True, description=desc, epilog=ep, formatter_class=argparse.RawTextHelpFormatter)
+        # Define dummy dict to keep compatibile with command-line usage
+        widget_dir = {}
+        widget_filesave = {}
+        widget_file = {}
+        widget_text = {}
+    # Required arguments
+    main_parser.add_argument('-i', '--input', metavar='/some/path', type=str, required=True,
+                        help='Path to the input folder. The renaming will be done recursively.', **widget_dir)
+
+    # Optional general arguments
+    main_parser.add_argument('-v', '--verbose', action='store_true', required=False, default=False,
+                        help='Verbose mode (show more output).')
+
+
+    #== Parsing the arguments
+    args = main_parser.parse_args(argv) # Storing all arguments to args
+    
+    #-- Set variables from arguments
+    inputpath = fullpath(args.input)
+    rootfolderpath = inputpath
+    verbose = args.verbose
+
+    # -- Sanity checks
+    if os.path.isfile(inputpath): # if inputpath is a single file (instead of a folder), then define the rootfolderpath as the parent directory (for correct relative path generation, else it will also truncate the filename!)
+        rootfolderpath = os.path.dirname(inputpath)
+
+    if not os.path.isdir(rootfolderpath):
+        raise NameError('Specified input path does not exist. Please check the specified path')
+
+    #### Main program
+    print("== Ascii Path Renamer started ==")
+    print("Renaming from root path %s" % rootfolderpath)
+    count_files = 0
+    count_renamed_files = 0
+    for dirpath, filename in recwalk(unicode(rootfolderpath), topdown=False):  # IMPORTANT: need to supply a unicode path to os.walk in order to get back unicode filenames! Also need to walk the tree bottom-up (from leaf to root), else if we change the directories names before the dirs/files they contain, we won't find them anymore!
+        count_files += 1
+        if verbose: print("- Processing file %s\n" % os.path.join(dirpath, filename))
+        ascii_filename = unidecode.unidecode(filename)  # native alternative but may remove quotes and some characters: unicodedata.normalize('NFKD', filename).encode('ascii','ignore')
+        if ascii_filename != filename:  # check that the filename/directory was not already ascii only
+            if verbose: print("- Renaming non-ascii file/dir %s to %s\n" % (filename, ascii_filename))
+            shutil.move(os.path.join(dirpath, filename), os.path.join(dirpath, ascii_filename))
+            count_renamed_files += 1
+        sys.stdout.write("\r%i files/folders done." % count_files)
+    print("\nAscii renaming is done, %i files/dirs renamed. Quitting now." % count_renamed_files)
+
+    return 0
+
+# Calling main function if the script is directly called (not imported as a library in another program)
+if __name__ == "__main__":  # pragma: no cover
+    sys.exit(main())
