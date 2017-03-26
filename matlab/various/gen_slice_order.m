ac0c426ac1deef024e7524a611c23136d7c6a281
Stephen L <lrq3000@gmail.com>
2017-03-26 03:12:47 +0200

    Add multiband EPI slice order + slice time offset support in gen_slice_order.m
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- matlab/various/gen_slice_order.m
+++ matlab/various/gen_slice_order.m
@@ -1,11 +1,24 @@
-function res=gen_slice_order(nslices, hstep, vstep, slice_order, unroll)
+function res=gen_slice_order(nslices, hstep, vstep, slice_order, unroll, multi, tr, slice_ids)
+% res=gen_slice_order(nslices, hstep, vstep, slice_order, unroll, tr, multi)
 % Compute a 2D slice order scheme based on given parameters
-% slice order can either be 'asc' or 'desc'.
+% slice_order can either be 'asc' or 'desc'.
+% multi > 0 enables multiband EPI. multi can be either 1 for rows or 2 for columns. This will return time points in seconds instead of slice number without multiband.
+% tr (in seconds) > 0 will return slice time offsets instead of slice number.
+% slice_ids is optional, you can provide your own custom slice indices with this argument, very useful for multiband with non linear schemes (if this exists?).
 % by Stephen Larroque from the Coma Science Group, 2017
-% licensed under MIT.
+% Licensed under MIT.
+%
 
 if ~exist('unroll', 'var')
     unroll = false;
+end
+
+if ~exist('multi', 'var')
+    multi = 0;
+end
+
+if ~exist('tr', 'var')
+    tr = 0;
 end
 
 % Compute horizontal vector
@@ -15,35 +28,51 @@ else
     hvec = [nslices:-hstep:1];  % could also use fliplr()...
 end %endif
 
-% Optimization using pre-allocation, but way less flexible and reliable...
-%vvec = zeros(1, hstep);
-%stepratio = hstep/vstep;
-%for v=1:vstep
-    %if v < vstep
-    %    endpos = v*round(stepratio);
-    %else
-    %    endpos = v*stepratio;
-    %end
-    %vvec(1, round(1+((v-1)*stepratio)):endpos) = v:vstep:hstep;
-%end
-
 % Compute vertical vector
 vvec = [];
 for v=1:vstep
     vvec = [vvec v:vstep:hstep];
-end
+end %endfor
 
 % Mix them both to get a matrix
-if strcmpi(slice_order, 'asc')
-    res =  bsxfun(@plus, repmat(hvec, hstep, 1), (vvec'-1));
-else
-    res =  bsxfun(@minus, repmat(hvec, hstep, 1), (vvec'-1));
+if ~exist('slice_ids', 'var')
+    if strcmpi(slice_order, 'asc')
+        slice_ids =  bsxfun(@plus, repmat(hvec, hstep, 1), (vvec'-1));
+    else
+        slice_ids =  bsxfun(@minus, repmat(hvec, hstep, 1), (vvec'-1));
+    end %endif
 end %endif
+res = slice_ids;
+
+% Multiband EPI: slices are acquired in parallel, either along rows or columns axis
+if multi > 0
+    if multi == 1
+        % Columns are parallel
+        res = bsxfun(@minus, res, (hvec - 1));
+    elseif multi == 2
+        % Rows
+        res = bsxfun(@minus, res, (vvec - 1)');
+        res = bsxfun(@minus, res, (hvec - 1));
+        res = bsxfun(@plus, res, ([1:size(res, 2)] - 1));
+    end %endif
+end %endif
+
+if tr > 0
+    % Convert to time offset seconds
+    res = (res-1) .* (tr/max(max(res)));
+end
 
 % Unroll if necessary into one horizontal vector (as required by SPM)
 if unroll
-    res = res';
-    res = res(:)';
-end
+    if multi == 0
+        res = res';
+        res = res(:)';
+    else
+        % For multiband EPI, we use the index
+        [vals, idx] = sort(slice_ids(:));
+        res = res(:);
+        res = res(idx);
+    end %endif
+end %endif
 
 end %endfunction
