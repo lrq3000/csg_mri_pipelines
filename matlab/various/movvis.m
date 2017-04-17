22effaf78876de433125ca798862b5998865c72b
Stephen L <lrq3000@gmail.com>
2017-04-17 21:10:16 +0200

    movvis.m fix logic to scan all sessions + add support to show only one file (in a greater window)
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- matlab/various/movvis.m
+++ matlab/various/movvis.m
@@ -1,48 +1,61 @@
 function movvis(root_path)
 % Movement visualization of all subjects after preprocessing with ART
 % Folders tree structure must correspond to conn subjects loader expected structure (Condition/Subject/data/Session/restMotionCorrected/rp_*.txt)
-% v1.0
+% v1.1
 % by Stephen Larroque 2016-2017
 % License MIT
 
 close all;
 
-% Scan the root path to extract all conditions and subjects names
-fprintf('Loading conditions (subjects groups)...\n');
-% Extract conditions
-conditions = get_dirnames(root_path);
-conditions = conditions(~strcmp(conditions, 'JOBS')); % remove JOBS from the conditions
-% Extract subjects names from inside the conditions
-subjects = {};
-for c=1:length(conditions)
-    subjects{c} = struct('names', []); % associate the subjects names for each condition
-    subjn = get_dirnames(fullfile(root_path, conditions{c}));
-    subjects{c}.names = subjn;
-end
+% Single file mode, we show the graph for only this file
+if exist(root_path, 'file') == 2
+    fprintf('Loading realign movement file of one subject: %s\n', root_path);
+    if ~isempty(root_path)
+        sid = 1;
+        list_files{sid} = struct('condition', '', ...
+                                'name', '', ...
+                                'session', '', ...
+                                'file', root_path);
+    end
+% Folder mode, we scan all groups/subjects/sessions
+else
+    % Scan the root path to extract all conditions and subjects names
+    fprintf('Loading realign movement files from all groups, subjects and sessions...\n');
+    % Extract conditions
+    conditions = get_dirnames(root_path);
+    conditions = conditions(~strcmp(conditions, 'JOBS')); % remove JOBS from the conditions
+    % Extract subjects names from inside the conditions
+    subjects = {};
+    for c=1:length(conditions)
+        subjects{c} = struct('names', []); % associate the subjects names for each condition
+        subjn = get_dirnames(fullfile(root_path, conditions{c}));
+        subjects{c}.names = subjn;
+    end
 
-list_files = {};
-sid = 0;
-% For each group
-for c=1:length(conditions)
-    % For each subject
-    for s=1:length(subjects{c}.names)
-        sid = sid + 1;
-        sname = subjects{c}.names{s};
-        datapath = fullfile(root_path, conditions{c}, sname, 'data');
+    list_files = {};
+    sid = 0;
+    % For each group
+    for c=1:length(conditions)
+        % For each subject
+        for s=1:length(subjects{c}.names)
+            sname = subjects{c}.names{s};
+            datapath = fullfile(root_path, conditions{c}, sname, 'data');
 
-        % For each session
-        sessions = get_dirnames(datapath);
-        for isess=1:length(sessions)
-            spath = fullfile(datapath, sessions{isess}, 'rest', 'restMotionCorrected');
-            % Extract realign motion txt file (from SPM)
-            sfile = regex_files(spath, '^rp_.+\.txt$');
+            % For each session
+            sessions = get_dirnames(datapath);
+            for isess=1:length(sessions)
+                spath = fullfile(datapath, sessions{isess}, 'rest', 'restMotionCorrected');
+                % Extract realign motion txt file (from SPM)
+                sfile = regex_files(spath, '^rp_.+\.txt$');
 
-            % Add this file only if found (eg, do not add mprage folders)
-            if ~isempty(sfile)
-                list_files{sid} = struct('condition', conditions{c}, ...
-                                        'name', sname, ...
-                                        'session', sessions{isess}, ...
-                                        'file', sfile);
+                % Add this file only if found (eg, do not add mprage folders)
+                if ~isempty(sfile)
+                    sid = sid + 1;
+                    list_files{sid} = struct('condition', conditions{c}, ...
+                                            'name', sname, ...
+                                            'session', sessions{isess}, ...
+                                            'file', sfile);
+                end
             end
         end
     end
