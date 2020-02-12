7f89c2df526879513b07acbc9fbe22a31bd1f671
Stephen L <lrq3000@gmail.com>
2020-02-12 20:39:58 +0100

    use gui after addpath spm
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- preprocessing/smri/vbm_script_preproc_csg.m
+++ preprocessing/smri/vbm_script_preproc_csg.m
@@ -24,7 +24,7 @@ function vbm_script_preproc_csg()
 % You also need Python (and add it to the PATH! Must be callable from cmd.exe with a simple "python" command) and PILLOW (not PIL! Just do `conda install pillow` or `pip install pillow`) to generate the final stitched image, but if you want to do it yourself Python is not needed.
 %
 % STEPHEN KARL LARROQUE
-% v1.3.9
+% v1.3.10
 % First version on: 2017-01-24 (first version of script based on batch from predecessors)
 % 2017-2020
 % LICENSE: MIT
@@ -73,20 +73,6 @@ if parallel_processing
     num_cores = 0; % disabling CAT12 parallel processing if we parallelize outside
 end
 
-% use spm gui to select files
-if strcmpi(rootpath_single, 'gui') == 1
-    rootpath_single = spm_select(1, 'image', 'Select nifti file of structural (FLAWS INV2 or MPRAGE)');
-    if isempty(rootpath_single)
-        error('No file selected, exiting');
-    end
-end
-if strcmpi(rootpath_multi, 'gui') == 1
-    rootpath_multi = spm_select(1, 'dir', 'Select directory of structurals in BIDS-like organization')
-    if isempty(rootpath_multi)
-        error('No directory selected, exiting');
-    end
-end
-
 % --- Start of main script
 fprintf(1, '\n=== VBM PREPROCESSING AND ANALYSIS ===\n');
 % Temporarily restore factory path and set path to SPM and its toolboxes, this avoids conflicts when having different versions of SPM installed on the same machine
@@ -101,6 +87,22 @@ end
 if autoreorient
     % For autoreorient script, we need to add the SPM12 OldNorm toolbox in the path
     addpath(fullfile(path_to_spm, 'toolbox', 'OldNorm'));
+end
+
+% use spm gui to select files
+if strcmpi(rootpath_single, 'gui') == 1
+    rootpath_single = spm_select(1, 'image', 'Select nifti file of structural (FLAWS INV2 or MPRAGE)');
+    if isempty(rootpath_single)
+        path(bakpath); % restore the path to the previous state
+        error('No file selected, exiting');
+    end
+end
+if strcmpi(rootpath_multi, 'gui') == 1
+    rootpath_multi = spm_select(1, 'dir', 'Select directory of structurals in BIDS-like organization')
+    if isempty(rootpath_multi)
+        path(bakpath); % restore the path to the previous state
+        error('No directory selected, exiting');
+    end
 end
 
 % Start logging
