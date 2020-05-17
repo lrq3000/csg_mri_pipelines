91a07562fd6f315b9aff777cc9ae311a81ead8ae
Stephen L <lrq3000@gmail.com>
2020-05-17 07:19:29 +0200

    move pathmatcher to its own submodule
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- utils/pathmatcher/pathmatcher_pyinstaller-win64.bat
+++
@@ -1,3 +0,0 @@
-pyinstaller --onedir pathmatcher_pyinstaller-win64.spec > pyinstaller-log.txt 2>&1 & type pyinstaller-log.txt
-pyi-archive_viewer dist\pathmatcher.exe -r -b > pyinstaller-dependencies.txt
-pause
--- utils/pathmatcher/reorienthelper.m
+++
@@ -1,6 +0,0 @@
-function reorienthelper(p)
-% reorienthelper(path)
-% Launch SPM12 Display dialog on the given nifti file, with contextual menu (right-click) enabled
-    spm_image('display', p);
-    spm_orthviews('AddContext');
-end %endfunction
